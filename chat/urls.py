f8fae6e1f7b868e6e54cf091a00c3584d333fb0c
Wajahat Ahmed <wajahat.ahmed245@gmail.com>
2019-07-02 14:44:23 +0500

    wajahat

---
+++ chat/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.urls import path, include,re_path
+
+from . import views
+
+app_name = 'chat'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+
+]
